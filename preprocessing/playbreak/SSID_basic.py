cf6bf047db30312c684641c99da337f5280fb4ce
liutingxi <liutingxi1993@outlook.com>
2017-05-02 11:17:54 +0800

    predict test

--- preprocessing/playbreak/SSID_basic.py
+++ preprocessing/playbreak/SSID_basic.py
@@ -1,14 +1,18 @@
 from keras.preprocessing.image import ImageDataGenerator
 import os,sys
+import numpy as np
 
 model_path = os.path.join('..','models','keras','models')
 sys.path.append(model_path)
 
 import lenet
+from imagenet_utils import preprocess_input
+from keras.preprocessing import image as keras_image
 
 weights_path = os.path.join('..', 'models', 'keras', 'weights', 'weights_basic.h5')
 #model.load_weights(weights_path)
-model = lenet.LeNet(weights_path=weights_path)
+#model = lenet.LeNet(weights_path=weights_path)
+model = lenet.LeNet(weights_path)
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
@@ -47,10 +51,25 @@ validation_generator = test_datagen.flow_from_directory(
 model.fit_generator(
         train_generator,
         samples_per_epoch=2000,
-        nb_epoch=100,
+        nb_epoch=0,
         validation_data=validation_generator,
         nb_val_samples=800)
+
 model.save_weights(weights_path)  # always save your weights after training or during training
+
+files = os.listdir('test-image')
+
+index = 0
+for file in files:
+    img = keras_image.load_img('test-image/' + file, target_size=(256, 256))
+    x = keras_image.img_to_array(img, dim_ordering='tf')
+    x = np.expand_dims(x, axis=0)
+    #x = preprocess_input(x)
+    result = np.argmax(model.predict(x))
+    print result, index/3+1
+    index += 1
+
+
 
 
 
